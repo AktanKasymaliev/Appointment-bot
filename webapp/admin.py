a68fb10d4cf046f8717ddadfc5a750b4af146ddf
AktanKasymaliev <dizzygame.dg@gmail.com>
2022-05-03 15:45:00 +0600

    added admin

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,11 @@
+import models
+
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(models.Applicant)
+admin.site.register(models.Card)
+admin.site.register(models.Appointment)
+admin.site.register(models.EmailAccount)
+admin.site.register(models.VFSAccount)
+admin.site.register(models.Settlement)
+admin.site.register(models.CrawlAttempt)
